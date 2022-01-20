3d5050be53d6adafef47dcdaf76266018d18586a
Kristin Ponsonby <kristinponsonby@gmail.com>
2022-01-19 20:39:42 -0500

    migrate db

--- db/migrate/20220119020229_create_fruits.rb
+++ db/migrate/20220119020229_create_fruits.rb
@@ -1,7 +1,10 @@
 class CreateFruits < ActiveRecord::Migration[5.2]
   def change
     create_table :fruits do |t|
-
+      t.string :genus
+      t.string :name
+      t.string :family
+      t.string :order
       t.timestamps
     end
   end
